a6f81f9db4fb10ec50fb9bd8a08a5306658fb25b
Alex Iwoh <71348624+alexiwoh@users.noreply.github.com>
2025-04-03 02:25:23 -0400

    small refactor

--- app/PdfBot/constants/embedding.py
+++ app/PdfBot/constants/embedding.py
@@ -3,4 +3,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 NUMBER_OF_SOURCES_DISPLAY = 3
 NUMBER_TOP_SOURCES = 6
 
-EMBEDDING_MODEL = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+EMBEDDING_MODEL = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2",
+    encode_kwargs={"normalize_embeddings": True}
+)
